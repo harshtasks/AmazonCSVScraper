916cac0b215961e90661e14b8a7ca91e0c5fa10e
harshtasks <109753787+harshtasks@users.noreply.github.com>
2022-07-26 00:30:04 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,71 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import extension
+
+# Either have chromedriver in path or initialize a service
+service = Service(executable_path='./webdriver/chromedriver.exe')
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--window-size=640,560")
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+data = extension.read_csv('Amazon Scraping - Sheet1')
+scraped = []
+
+for item in data:
+    item_id = item[1]
+    asin = item[2]
+    country = item[3]
+    url = 'https://amazon.{}/dp/{}'.format(country, asin)
+    driver.get(url)
+    page = BeautifulSoup(driver.page_source, "html.parser")
+    title = page.select_one('#productTitle')
+    # Use the product title selector to check whether a product exists
+    if title is None:
+        print('URL not avaiable: {}'.format(url))
+        continue
+    else:
+        title = title.text.strip()
+
+    # Different pages use different kind of image placement and selectors
+    img = page.select_one('#landingImage') or page.select_one('#imgTagWrapperId img') or page.select_one('#imgBlkFront')
+    if img is not None:
+        img = img['src']
+    else: img = ""
+
+    price = page.select_one('#price')
+    if price is None:
+        price = page.select_one('.swatchElement a span.a-color-base')
+        if price is None:
+            price = page.select_one('#corePrice_feature_div span.a-price .a-offscreen').text
+        else:
+            price = price.text.strip()
+    else:
+        price = price.text
+
+    # The details list is stored in key value pairs
+    details_list = page.select('div#detailBullets_feature_div>ul span.a-list-item')
+    details = []
+    for detail in details_list:
+        key = detail.select_one('span.a-text-bold').text.strip()
+        key = key[:key.find('\n')]
+        value = detail.select_one('span.a-text-bold+span').text.strip()
+        details.append({key: value})
+
+    item_info = {
+        "id": item_id,
+        "asin": asin,
+        "country": country,
+        "title": title,
+        "price": price,
+        "image": img,
+        "details": details
+    }
+    scraped.append(item_info)
+
+driver.close()
+
+extension.save_json(scraped)
